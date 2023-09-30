c351267aeef5c1d4bef8ca1a869b76a2c40dcda6
Igor Lunyakin <lunyaigor484@gmail.com>
2023-09-30 15:46:12 +0300

    fixed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,18 +3,18 @@ import { StyledOverlay, StyledModal } from './Modal.styled';
 
 export const Modal = ({ onClose, largeImageURL }) => {
   useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+
     document.addEventListener('keydown', handleKeyDown);
 
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, []);
-
-  const handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      onClose();
-    }
-  };
+  });
 
   const handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
